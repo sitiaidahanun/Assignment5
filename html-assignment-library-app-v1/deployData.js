78d41f74e9c78e71d56bcc903fc08232eeb2e5ec
Siti Aida Hanun <siti.22044@mhs.unesa.ac.id>
2024-10-16 20:24:20 +0700

    Initial commit

---
+++ html-assignment-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Siti Aida Hanun'; // TODO: replace this
+
+const studentId = 'CAMP123456789'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https:voluble-tapioca-bd927c.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
